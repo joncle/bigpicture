253a6de1f9a2e1700ed71d7568d3fac4b6850dad
basjo <nouvellecollection@gmail.com>
2013-12-14 01:32:55 +0100

    Update bigpicture0.py

--- bigpicture0.py
+++ bigpicture0.py
@@ -10,18 +10,18 @@ DONE :
  * ALT + RIGHT click on a Textbox : DELETE it   
  * Save/read to/from XML 
  * If Entry has no text, destroy it from the Entry list
- * GoogleMaps-like  Zoom + / -  and N, S, E, W arrows    
+ * GoogleMaps-like  Zoom + / -  and N, S, E, W arrows
+ * Unicode encoding in writexml()
+ * Entry() replaced by Text() -> multiline!      
 
 TODO :
  * Add a "Redimension / move"  Entry feature   
- * Replace "Entry" widget by "Text" widget (multiline)  => needs to auto-dimension the widget while typing text => needs proper counting of lines
- * Replace X, Y by root.winfo_width(),  but then initialization of the widgets must be done ONCE mainloop has started 
- * Unicode support 
 """ 
 
 import Tkinter as Tk
 from PIL import Image, ImageTk
 from xml.dom import minidom
+import tkFont
 
 coef = 1.732    # sqrt(3)
 currentx=0.
@@ -31,41 +31,64 @@ b1pressed = False
 xold = 0
 yold = 0
 
-class Texte(Tk.Entry):        #subclass of Entry
+class Texte(Tk.Text):        #subclass of Entry
     def __init__(self, event=None, x=None, y=None, size=None, txt=None, *args, **kwargs):
-        Tk.Entry.__init__(self, master=c, bd=0, *args, **kwargs)
+        Tk.Text.__init__(self, master=c, bd=0, *args, **kwargs)
         if event != None:
-            self.x = currentx + event.x / X * currentzoom
-            self.y = currenty + event.y / Y * currentzoom
+            self.x = currentx + event.x / X() * currentzoom
+            self.y = currenty + event.y / Y() * currentzoom
             self.size = 20 * currentzoom
         else:
             self.x = x
             self.y = y
             self.size = size
-            self.insert(0, txt) 
+            self.insert(Tk.INSERT, txt) 
+        
         self.focus_force()
-        self.bind("<Return>", lambda e: root.focus_force() )
+        
         self.bind("<Alt-Button-3>", lambda event: self.destroy())
         self.bind("<FocusOut>", lambda event: self.destroyifempty())
+        
+        bindtags = list(self.bindtags())
+        bindtags.insert(2, "custom")
+        self.bindtags(tuple(bindtags))
+        self.bind_class("custom", "<Key>", self.draw)
+        
         self.draw()
         
     def destroyifempty(self):
-        if self.get() == '':
+        if self.get(1.0, "end-1c") == '':
             self.destroy()
         
-    def draw(self):
-        newx = int((self.x-currentx)/currentzoom*X)
-        newy = int((self.y-currenty)/currentzoom*Y)
+    def draw(self, event=None):
+        if event:                 # sinon self peut etre n importe quoi!
+            self = event.widget
+            if event.keycode in [16,17,18]:     # ctrl, maj, alt => don't redraw
+                return
+        #font
         displaysize = int(self.size/currentzoom)
-        if newx > 2*X or newx < - 2*X or newy > 2*Y or newy < - 2*Y:  # out of the display window, ideally we should hide these widgets! 
-           displaysize = 1                 
-           self.place(x=2*X,y=2*Y)
-        else:
-           self.place(x=newx,y=newy)
-        
         if displaysize < 1:
           displaysize = 1
-        self.config(font=("Helvetica Neue LT Com 55 Roman",displaysize))
+        self.font = tkFont.Font(family="Helvetica Neue LT Com 55 Roman",size=displaysize)
+
+        #position
+        newx = int((self.x-currentx)/currentzoom*X())
+        newy = int((self.y-currenty)/currentzoom*Y())
+        if newx > 2*X() or newx < - 2*X() or newy > 2*Y() or newy < - 2*Y():  # out of the display window, ideally we should hide these widgets! 
+           displaysize = 1        
+           newx, newy=2*X(), 2*Y()         
+        
+        #size
+        width=0
+        lines=0
+        for line in self.get(1.0, "end-1c").split("\n"):
+            width=max(width,self.font.measure(line))
+            lines += 1
+       
+        #place
+        self.config(height=lines, font=self.font)
+        self.place(x=newx, y=newy, width=width+10)
+        
   
 # Redraw all textboxes
 def redraw():
@@ -82,7 +105,7 @@ def writexml():
     gz = doc.createElement('Currentzoom'); general.appendChild(gz); gz.appendChild(doc.createTextNode(str(currentzoom)))
     for e in c.children.values():
         item = doc.createElement('Item'); base.appendChild(item)
-        text = doc.createElement('Text'); item.appendChild(text); text.appendChild(doc.createTextNode(e.get()))
+        text = doc.createElement('Text'); item.appendChild(text); text.appendChild(doc.createTextNode(e.get(1.0, "end-1c").rstrip("\n ").encode('utf-8')))   # enlever les espaces et newlines, encoder en utf8 (regle les problemes accents)
         x = doc.createElement('x'); item.appendChild(x); x.appendChild(doc.createTextNode(str(e.x)))
         y = doc.createElement('y'); item.appendChild(y); y.appendChild(doc.createTextNode(str(e.y)))
         size = doc.createElement('size'); item.appendChild(size); size.appendChild(doc.createTextNode(str(e.size)))
@@ -103,14 +126,11 @@ def readxml():
         
         itemlist = xmldoc.getElementsByTagName('Item') 
         for s in itemlist:
-            try:
               text=s.getElementsByTagName('Text')[0].firstChild.nodeValue
               x=float(s.getElementsByTagName('x')[0].firstChild.nodeValue)
               y=float(s.getElementsByTagName('y')[0].firstChild.nodeValue)
               size=float(s.getElementsByTagName('size')[0].firstChild.nodeValue)
               Texte(x=x,y=y,size=size,txt=text)
-            except:
-              pass
     except:
         pass
               
@@ -136,8 +156,8 @@ def zoomonwidget():
     global currentzoom, currentx, currenty
     a = c.focus_get().winfo_x()
     b = c.focus_get().winfo_y()
-    middlex = currentx + a / X * currentzoom
-    middley = currenty + b / Y * currentzoom
+    middlex = currentx + a / X() * currentzoom
+    middley = currenty + b / Y() * currentzoom
     currentzoom /= coef
     currentx = middlex - currentzoom / 2
     currenty = middley - currentzoom / 2
@@ -168,15 +188,15 @@ def ctrlb1down(event):
     global b1pressed, xold, yold
     b1pressed, xold, yold = True, event.x, event.y
     
-def ctrlb1up(event):
+def b1up(event):
     global b1pressed
     b1pressed = False
    
 def motion(event):
     global currentzoom, currentx, currenty, xold, yold
     if b1pressed:
-        currentx += (xold - event.x) / X * currentzoom
-        currenty += (yold - event.y) / Y * currentzoom
+        currentx += (xold - event.x) / X() * currentzoom
+        currenty += (yold - event.y) / Y() * currentzoom
         xold, yold = event.x, event.y
         redraw() 
     
@@ -189,9 +209,8 @@ root.rowconfigure(0, weight=1)
 c = Tk.Frame(root)
 c.configure(background='white')
 c.grid(sticky='NESW')
-
-X=1100.  # default window width,  another solution : X = lambda: 1100. #float(root.winfo_width()) but this is not accessible before mainloop  
-Y=600.   # default window height, another solution : Y = lambda: 600. #float(root.winfo_height())    => objects creation should not be done *before mainloop*
+X = lambda: float(root.winfo_width())
+Y = lambda: float(root.winfo_height())
 
 # Navigation buttons
 image_file = Image.open("buttons.png")
@@ -220,11 +239,14 @@ c.bind("<ButtonPress-1>", lambda e: Texte(event=e))
 root.bind('<Control-=>', lambda e: zoomplus())
 root.bind('<Control-)>', lambda e: zoomminus())            
 root.bind('<Control-minus>', lambda e: zoomminus())
+root.bind('<Control-à>', lambda e: zoomonwidget())
 c.bind("<Motion>", motion)
 c.bind("<Control-ButtonPress-1>", ctrlb1down)
-c.bind("<Control-ButtonRelease-1>", ctrlb1up)
+c.bind("<ButtonRelease-1>", b1up)
 root.protocol("WM_DELETE_WINDOW", quit)
-readxml()
+root.bind("<Configure>", lambda e: redraw())  #window resize
+
+root.after(10,readxml)  # done only 10ms after mainloop() has started, prevents problem with X(), Y() = 1.0 before mainloop()
 
 # Main loop 
 root.mainloop()
